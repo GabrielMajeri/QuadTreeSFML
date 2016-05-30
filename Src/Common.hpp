c4af665a45b75cf09e68299b736463297a31653d
Gabriel M <gabriel.majeri6@gmail.com>
2016-05-30 21:15:54 +0300

    Added comments

--- Src/Common.hpp
+++ Src/Common.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+/// Common precompiled header with useful includes
+
+/// Standard Library Includes
 #include <memory>
 #include <iostream>
 #include <chrono>
@@ -7,8 +10,10 @@
 #include <array>
 #include <random>
 
+/// Non Copyable utility class
 #include "NonCopyable.hpp"
 
+/// The Simple and Fast Multimedia Library
 #include <SFML/System.hpp>
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
